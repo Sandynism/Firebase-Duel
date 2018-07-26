602237113000206ef8eedc01994aa9e855ecaa81
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-07-26 15:25:36 -0400

    testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+//document ready
+$(function() { 
 let config = {
     apiKey: "AIzaSyBoLOrRydq3VrF-i3YZlR9d5ihLBWkINZY",
     authDomain: "learn-firebase-3883f.firebaseapp.com",
@@ -8,29 +10,80 @@ let config = {
 }
 firebase.initializeApp(config);
 
-// Create a variable to reference the database.
-let database = firebase.database();
-// let playersRef = new Firebase('https://learn-firebase-3883f.firebaseio.com/playersRef')
-// let turnRef = new Firebase('https://learn-firebase-3883f.firebaseio.com/turn')
-// let chatRef = new Firebase('https://learn-firebase-3883f.firebaseio.com/chat')
+//Game variables
+let player = {}
+let otherPlayer = {}
+let p1wins
+let p2wins
+let p1losses
+let p2losses 
+let ties
+let choices = ['rock', 'paper', 'scissor']
 
-// Link to Firebase Database for viewer tracking.
+// Links to Firebase Database
+let database = firebase.database()
 let connectionsRef = database.ref('/connections')
 let connectedRef = database.ref('.info/connected')
+let playersRef = database.ref('/playersRef')
+let turnRef = database.ref('/turn')
+let chatRef = database.ref('/chat')
 
+
+// function onlineStatus() 
+//Listening for players.
 connectedRef.on("value", function (snapshot) {
     if (snapshot.val()) {
         let connection = connectionsRef.push(true)
         connection.onDisconnect().remove()
+        playerInfo(connection.key)
     }
 })
-
 // Add ourselves to presence list when online.
 connectionsRef.on("value", function (snapshot) {
     $("#connected-viewers").text(snapshot.numChildren())
 })
 
-// var ref = firebase.database().ref()
+// let playerInfo = () => 
+//     name = $("#player-name").val().trim()
+
+//On submit-name, the player's name is pushed into database
+$("#submit-name").on('click', function () {
+    event.preventDefault()
+    name = $("#player-name").val()
+    // let player = playerInfo() ??
+    // database.ref().push({
+    //     name: name,
+    //     dateAdded: firebase.database.ServerValue.TIMESTAMP
+    // })
+})
+
+// playersRef.on('child_added', function(childSnapshot) {
+//     let key = childSnapshot.key()
+//     name[key] = childSnapshot.val().name
+    
+// })
+
+let playerInfo = (id) => { 
+    $("#submit-name").on('click', function () {
+        if ($("#player-name").val()) {
+            player.id = id
+            player.name = $("#player-name").val().trim()
+            gameDisplay()
+        }
+    })
+}
+
+function gameDisplay() {
+    $(".welcome-screen").addClass("hide")
+    $(".game-diretions").removeClass("hide")
+    $(".game-page").removeClass("hide")
+}
+
+
+
+}) //end of page
+
+// let ref = firebase.database().ref()
 
 // ref.on("value", function (snapshot) {
 //     output.innerHTML = JSON.stringify(snapshot.val(), null, 2)
@@ -40,76 +93,15 @@ connectionsRef.on("value", function (snapshot) {
 // turnRef.onDisconnect().remove();
 // chatRef.onDisconnect().remove();
 
+// Loads chat messages history and listens for upcoming ones. (Taken from Firebase)
+// function loadMessages() {
+//     // Loads the last 12 messages and listen for new ones.
+//     let callback = function(snapshot) {
+//       let data = snapshot.val()
+//       displayMessage(snapshot.key, data.name, data.text)
+//     };
+  
+//     firebase.database().ref('/messages/').limitToLast(12).on('child_added', callback);
+//     firebase.database().ref('/messages/').limitToLast(12).on('child_changed', callback);
+//   }
 
-// Chat object
-// let chat = {
-//     message: '',
-//     listeners: function () {
-//         // Send button click
-//         $('#send-message').on('click', function (event) {
-//             chat.getMessage();
-//             return false;
-//         });
-//         // Show message when received
-//         chatRef.on('child_added', function (childSnapshot) {
-//             // Get name and message
-//             let playerName = childSnapshot.val().name;
-//             let message = childSnapshot.val().message;
-//             // Show message
-//             chat.showMessage(playerName, message);
-//         });
-//     },
-//     getMessage: function () {
-//         var input = $('#message-input');
-//         // Get message then clear it
-//         chat.message = input.val();
-//         input.val('');
-//         // Send data to database if player has name
-//         if (player !== undefined) {
-//             chat.sendMessage();
-//         }
-//     },
-//     sendMessage: function () {
-//         let obj = {};
-//         obj['name'] = name[player];
-//         obj['message'] = chat.message;
-//         chatRef.push(obj);
-//     },
-//     sendDisconnect: function (key) {
-//         let obj = {};
-//         obj['name'] = name[key];
-//         obj['message'] = ' has disconnected.';
-//         chatRef.push(obj);
-//     },
-//     showMessage: function (playerName, message) {
-//         // Auto scroll to bottom variables
-//         let messages = document.getElementById('messages');
-//         let isScrolledToBottom = messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1;
-//         // Create p with display string
-//         let $p = $('<p>');
-//         if (message == ' has disconnected.' && player !== undefined) {
-//             $p.text(playerName + message);
-//             $p.css('background', 'gray');
-//         } else if (player !== undefined) {
-//             $p.text(playerName + ': ' + message);
-//         }
-//         // If player 1 -> blue text
-//         if (name[1] == playerName) {
-//             $p.css('color', 'green');
-//             // If player 2 -> red text
-//         } else if (name[2] == playerName) {
-//             $p.css('color', 'purple');
-//         }
-//         // Append message
-//         if ($p.text() !== '') {
-//             $('#messages').append($p);
-//         }
-//         // Auto scroll to bottom
-//         if (isScrolledToBottom) {
-//             messages.scrollTop = messages.scrollHeight - messages.clientHeight;;
-//         }
-//     }
-// }
-
-// // Start chat
-// chat.listeners();
